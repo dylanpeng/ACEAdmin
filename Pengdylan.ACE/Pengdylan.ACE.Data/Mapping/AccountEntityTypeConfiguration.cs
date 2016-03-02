3b40930e077638e1ab2b2dfa4ec71e88e1fc6604
peng_dylan <peng_dylan@sina.com>
2016-03-02 18:04:00 +0800

    2016-3-2    Detail:add mapping file

---
+++ Pengdylan.ACE/Pengdylan.ACE.Data/Mapping/AccountEntityTypeConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.ModelConfiguration;
+
+namespace Pengdylan.ACE.Data.Mapping
+{
+    public class AccountEntityTypeConfiguration : EntityTypeConfiguration<Account>
+    {
+        public AccountEntityTypeConfiguration()
+        {
+            ToTable("Account");
+        }
+    }
+}
