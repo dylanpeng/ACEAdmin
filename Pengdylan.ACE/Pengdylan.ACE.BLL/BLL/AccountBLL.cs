fae9888587081896e0f80c01c94c0be2325fc0ca
peng_dylan <peng_dylan@sina.com>
2016-06-24 16:44:19 +0800

    2016-6-24 unity DLL及相关文件签入

---
+++ Pengdylan.ACE/Pengdylan.ACE.BLL/BLL/AccountBLL.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.Configuration;
+using Pengdylan.ACE.DAL.DALImplement;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pengdylan.ACE.BLL
+{
+    public static class AccountBLL
+    {
+        public static bool Add()
+        {
+            IUnityContainer container = new UnityContainer();//获取指定名称的配置节  
+            UnityConfigurationSection section = (UnityConfigurationSection)ConfigurationManager.GetSection("unity");
+            container.LoadConfiguration(section, "MyContainer");
+            var accountDAL = container.Resolve<IAccountDAL>();
+            return accountDAL.Add();
+        }
+    }
+}
---
+++ Pengdylan.ACE/Pengdylan.ACE.DAL/DAL/AccountDAL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Pengdylan.ACE.DAL.DALImplement;
+using Pengdylan.ACE.Data;
+
+namespace Pengdylan.ACE.DAL
+{
+    public class AccountDAL : IAccountDAL
+    {
+        public bool Add()
+        {
+            var context = new DataContext();
+            context.Account.Add(new Data.Account() { Name = "admin", Password = "123456" });
+            context.SaveChanges();
+            bool result = false;
+            return result;
+        }
+    }
+}
